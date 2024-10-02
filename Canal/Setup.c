fa614cf90b1604235dc45c6ec34a9a94dbe00214
Josue3108 <guille4567mo@gmail.com>
2024-10-02 01:01:03 -0600

    Begin setup to create a canal from a file

---
+++ Canal/Setup.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Structs/Canal.h"
+#include "Structs/Canal.h"
+
+struct Canal setup(int lenght, char mode, int speed, int w, int time) {
+    
+}
--- Canal/Structs/Canal.h
+++ Canal/Structs/Canal.h
@@ -3,8 +3,12 @@
 
 struct Canal {
     short size;
+    short speed;
     char control[1];
     short time;
+    short w;
     struct NodeList* list;
+    struct BoatList* right;
+    struct BoatList* left;
 };
 #endif
